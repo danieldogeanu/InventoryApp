468bca1f3881b8195ae43edf53a46686f4574675
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-22 16:39:38 +0300

    Added Inventory Contract for the SQLite Database

---
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,22 @@
+package com.danieldogeanu.android.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+public final class InventoryContract {
+
+    public static abstract class InventoryEntry implements BaseColumns {
+
+        // The name of the table.
+        public static final String TABLE_NAME = "inventory";
+
+        // The columns names for the table.
+        public static final String _ID = BaseColumns._ID;
+        public static final String COL_PRODUCT_NAME = "product_name";
+        public static final String COL_PRICE = "price";
+        public static final String COL_QUANTITY = "quantity";
+        public static final String COL_SUPPLIER_NAME = "supplier_name";
+        public static final String COL_SUPPLIER_PHONE = "supplier_phone";
+
+    }
+
+}
