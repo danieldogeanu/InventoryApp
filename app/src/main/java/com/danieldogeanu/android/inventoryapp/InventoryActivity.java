ea9ef84f8d4e9640a0af4e4c5b45ae4a4a62ec3e
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-23 16:22:48 +0300

    Added Intent to Open Editor Activity

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
@@ -1,7 +1,9 @@
 package com.danieldogeanu.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        FloatingActionButton fab = findViewById(R.id.inventory_fab);
+        fab.setOnClickListener(view -> {
+            Intent editorIntent = new Intent(InventoryActivity.this, EditorActivity.class);
+            startActivity(editorIntent);
+        });
+
     }
 }
