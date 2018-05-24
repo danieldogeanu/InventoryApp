f7f57d7c9753d659ad123068f85f1051fa13019d
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-24 21:06:31 +0300

    Set Up the ProductAdapter and Populated the Inventory List with Data

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.danieldogeanu.android.inventoryapp.data.Data;
+
+import java.util.ArrayList;
 
 public class InventoryActivity extends AppCompatActivity {
 
@@ -11,6 +16,17 @@ public class InventoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        // Get List View
+        ListView listView = findViewById(R.id.inventory_list);
+
+        // Get Data
+        Data data = Data.getInstance();
+        ArrayList<Product> products = data.queryData(InventoryActivity.this);
+
+        // Set the Adapter
+        ProductAdapter adapter = new ProductAdapter(InventoryActivity.this, products);
+        listView.setAdapter(adapter);
 
         FloatingActionButton fab = findViewById(R.id.inventory_fab);
         fab.setOnClickListener(view -> {
