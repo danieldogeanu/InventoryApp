1ab2efbfd6cef85717cc9742c3eaa658a1d8101b
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-27 17:19:57 +0300

    Added Method to Save Product into Database from Editor Activity

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/EditorActivity.java
@@ -1,16 +1,45 @@
 package com.danieldogeanu.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.danieldogeanu.android.inventoryapp.data.Data;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private Data mData;
+
+    private EditText mProductNameEditText;
+    private EditText mProductAuthorEditText;
+    private EditText mProductPriceEditText;
+    private EditText mProductQuantityEditText;
+    private EditText mSupplierNameEditText;
+    private EditText mSupplierPhoneEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mData = Data.getInstance();
+
+        mProductNameEditText = findViewById(R.id.product_name);
+        mProductAuthorEditText = findViewById(R.id.product_author);
+        mProductPriceEditText = findViewById(R.id.product_price);
+        mProductQuantityEditText = findViewById(R.id.product_quantity);
+        mSupplierNameEditText = findViewById(R.id.supplier_name);
+        mSupplierPhoneEditText = findViewById(R.id.supplier_phone);
+
+        FloatingActionButton saveFab = findViewById(R.id.save_fab);
+        saveFab.setOnClickListener(view -> {
+            saveProduct();
+            finish();
+        });
+
     }
 
     @Override
@@ -23,13 +52,29 @@ public class EditorActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_save:
-                // Do Stuff
+                saveProduct();
+                finish();
                 return true;
             case R.id.action_delete:
                 // Do Stuff
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void saveProduct() {
+        String productNameString = mProductNameEditText.getText().toString().trim();
+        String productAuthorString = mProductAuthorEditText.getText().toString().trim();
+        String productPriceString = mProductPriceEditText.getText().toString().trim();
+        String productQuantityString = mProductQuantityEditText.getText().toString().trim();
+        String supplierNameString = mSupplierNameEditText.getText().toString().trim();
+        String supplierPhoneString = mSupplierPhoneEditText.getText().toString().trim();
+
+        float productPriceFloat = Float.parseFloat(productPriceString);
+        int productQuantityInt = Integer.parseInt(productQuantityString);
+
+        Product product = new Product(productNameString, productAuthorString, productPriceFloat, productQuantityInt, supplierNameString, supplierPhoneString);
+        mData.insertData(EditorActivity.this, product);
     }
 
 }
