5a11e3c7b34b4f45ba5a9ec46d5082afae756000
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-27 17:44:03 +0300

    Added Intent to Open Editor Activity and Send Product ID

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/ProductAdapter.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/ProductAdapter.java
@@ -1,12 +1,14 @@
 package com.danieldogeanu.android.inventoryapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +26,7 @@ public class ProductAdapter extends ArrayAdapter<Product> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
             viewHolder = new ViewHolder();
+            viewHolder.itemCard = convertView.findViewById(R.id.item_card);
             viewHolder.productNameTextView = convertView.findViewById(R.id.item_product_name);
             viewHolder.productAuthorTextView = convertView.findViewById(R.id.item_product_author);
             viewHolder.productPriceTextView = convertView.findViewById(R.id.item_product_price);
@@ -37,6 +40,12 @@ public class ProductAdapter extends ArrayAdapter<Product> {
 
         Product currentProduct = getItem(position);
 
+        viewHolder.itemCard.setOnClickListener(view -> {
+            Intent editorIntent = new Intent(getContext(), EditorActivity.class);
+            editorIntent.putExtra("product_id", currentProduct.getProductID());
+            getContext().startActivity(editorIntent);
+        });
+
         viewHolder.productNameTextView.setText(currentProduct.getProductName());
         viewHolder.productAuthorTextView.setText(currentProduct.getProductAuthor());
         viewHolder.productPriceTextView.setText(Utils.formatPrice(currentProduct.getProductPrice()));
@@ -48,12 +57,13 @@ public class ProductAdapter extends ArrayAdapter<Product> {
     }
 
     private static class ViewHolder {
+        private RelativeLayout itemCard;
         private TextView productNameTextView,
-                productAuthorTextView,
-                productPriceTextView,
-                productQuantityTextView,
-                supplierNameTextView,
-                supplierPhoneTextView;
+        productAuthorTextView,
+        productPriceTextView,
+        productQuantityTextView,
+        supplierNameTextView,
+        supplierPhoneTextView;
     }
 
 }
