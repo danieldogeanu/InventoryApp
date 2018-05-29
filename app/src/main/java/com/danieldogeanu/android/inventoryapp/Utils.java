e578b8d51a224f46975a6afab8c451295bb25959
Daniel Dogeanu <contact@danieldogeanu.com>
2018-05-29 14:23:07 +0300

    Documented the Utils Class

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/Utils.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/Utils.java
@@ -7,14 +7,30 @@ import java.text.NumberFormat;
 import java.util.Currency;
 import java.util.Locale;
 
+/**
+ * General utility methods used across the app.
+ */
 public final class Utils {
 
+    /** Private constructor, so we can't instantiate the class. */
     private Utils() {}
 
+    /**
+     * Method that displays short Toast messages in the app.
+     * @param context The context from which this method is called.
+     * @param message The message to show in the Toast.
+     */
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Method to format the price, according to the user's locale.
+     * It allows only two decimal digits after the point and adds currency symbol.
+     * Please don't use this method to insert the price into the database!
+     * @param rawPrice The raw price received from the database.
+     * @return Returns the formatted price.
+     */
     public static String formatPrice(float rawPrice) {
         Locale locale = Locale.getDefault();
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
@@ -24,6 +40,12 @@ public final class Utils {
         return numberFormat.format(rawPrice);
     }
 
+    /**
+     * Method to format the quantity, according to the user's locale.
+     * Please don't use this method to insert the quantity into the database!
+     * @param rawQuantity The raw quantity received from the database.
+     * @return Returns the formatted quantity.
+     */
     public static String formatQuantity(float rawQuantity) {
         Locale locale = Locale.getDefault();
         NumberFormat numberFormat = NumberFormat.getIntegerInstance(locale);
