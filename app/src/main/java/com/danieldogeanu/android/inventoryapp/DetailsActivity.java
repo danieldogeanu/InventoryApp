abd263aacaec9b046d7aa9a9986f932f8aa9e899
Daniel Dogeanu <contact@danieldogeanu.com>
2018-06-05 11:01:01 +0300

    Added Menu Items to the Details Activity

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/DetailsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.danieldogeanu.android.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                // Do something.
+                return true;
+            case R.id.action_delete:
+                // Do something.
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
