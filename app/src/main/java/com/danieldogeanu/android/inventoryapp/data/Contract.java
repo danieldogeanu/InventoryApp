e1acd1b59da4cb9f771e6f3d2ccb3eb9147c6943
Daniel Dogeanu <contact@danieldogeanu.com>
2018-06-06 19:07:19 +0300

    Defined Constants in the Contract Class for Later Use in the Provider

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/data/Contract.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/data/Contract.java
@@ -1,5 +1,7 @@
 package com.danieldogeanu.android.inventoryapp.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +9,33 @@ import android.provider.BaseColumns;
  */
 public final class Contract {
 
+    private Contract() {}
+
+    // The CONTENT_AUTHORITY is a name for the entire content provider, similar to the relationship
+    // between a domain name and its website. A convenient string to use for the content authority
+    // is the package name for the app, which is guaranteed to be unique on the device.
+    public static final String CONTENT_AUTHORITY = "com.danieldogeanu.android.inventoryapp";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // The path (table name) appended to the base content URI.
+    public static final String PATH_TABLE = "products";
+
+    /**
+     * Inner class that defines constant values for the inventory database table.
+     * Each entry in the table represents a single product.
+     */
     public static abstract class TableEntry implements BaseColumns {
+
+        // The content URI to access the products data in the provider.
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TABLE);
+
+        // The MIME type of the CONTENT_URI for a list of products.
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TABLE;
+
+        // The MIME type of the CONTENT_URI for a single product.
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TABLE;
 
         // The name of the table.
         public static final String TABLE_NAME = "products";
