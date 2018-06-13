4d2e071677e2c4b16a81453ca35609cb85b0cb10
Daniel Dogeanu <contact@danieldogeanu.com>
2018-06-13 19:14:41 +0300

    Added insertDummyData Method to InventoryActivity Class

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/InventoryActivity.java
@@ -1,5 +1,6 @@
 package com.danieldogeanu.android.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
+import com.danieldogeanu.android.inventoryapp.data.Contract.TableEntry;
 import com.danieldogeanu.android.inventoryapp.data.Data;
 
 import java.util.ArrayList;
@@ -100,5 +102,31 @@ public class InventoryActivity extends AppCompatActivity {
         ProductAdapter adapter = new ProductAdapter(InventoryActivity.this, products);
         listView.setAdapter(adapter);
         listView.setEmptyView(emptyView);
+    }
+
+    /** Insert dummy (demo) product data into the database. */
+    private void insertDummyData() {
+        // Get dummy data string arrays.
+        String[] dummyProductTitles = getResources().getStringArray(R.array.dummy_prod_titles);
+        String[] dummyProductAuthors = getResources().getStringArray(R.array.dummy_prod_authors);
+        String[] dummyProductPrices = getResources().getStringArray(R.array.dummy_prod_prices);
+        String[] dummyProductQuantities = getResources().getStringArray(R.array.dummy_prod_quantity);
+        String[] dummySupplierNames = getResources().getStringArray(R.array.dummy_supl_names);
+        String[] dummySupplierPhones = getResources().getStringArray(R.array.dummy_supl_phones);
+
+        // Insert dummy data into the database.
+        for (int i = 0; i < dummyProductTitles.length; i++) {
+            // Extract dummy data and add it to ContentValues object.
+            ContentValues values = new ContentValues();
+            values.put(TableEntry.COL_PRODUCT_NAME, dummyProductTitles[i]);
+            values.put(TableEntry.COL_AUTHOR, dummyProductAuthors[i]);
+            values.put(TableEntry.COL_PRICE, Float.parseFloat(dummyProductPrices[i]));
+            values.put(TableEntry.COL_QUANTITY, Integer.parseInt(dummyProductQuantities[i]));
+            values.put(TableEntry.COL_SUPPLIER_NAME, dummySupplierNames[i]);
+            values.put(TableEntry.COL_SUPPLIER_PHONE, dummySupplierPhones[i]);
+
+            // Insert values into the database.
+            getContentResolver().insert(TableEntry.CONTENT_URI, values);
+        }
     }
 }
