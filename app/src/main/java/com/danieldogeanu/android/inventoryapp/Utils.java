bcdbe27bb35db5a90a39709dd4d802ca4af4204a
Daniel Dogeanu <contact@danieldogeanu.com>
2018-06-15 05:45:50 +0300

    Added Method to Show Toasts and Log Messages in the Utils Class

--- app/src/main/java/com/danieldogeanu/android/inventoryapp/Utils.java
+++ app/src/main/java/com/danieldogeanu/android/inventoryapp/Utils.java
@@ -1,6 +1,7 @@
 package com.danieldogeanu.android.inventoryapp;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.text.NumberFormat;
@@ -22,6 +23,19 @@ public final class Utils {
      */
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Method that displays short Toast and logs messages to the console.
+     * @param context The context from which this method is called.
+     * @param error Flag to show if it's an error or not.
+     * @param tag The tag title for the Logs.
+     * @param message The message to show in the Toast and Log to the console.
+     */
+    public static void showToastAndLog(Context context, boolean error, String tag, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        if (error) Log.e(tag, message);
+        else Log.i(tag, message);
     }
 
     /**
